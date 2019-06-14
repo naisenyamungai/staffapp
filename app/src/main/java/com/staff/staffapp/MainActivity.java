8979f77f10adf5455067e666145c6613253d6f7e
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-06-14 10:01:15 +0300

    created products landing page

--- app/src/main/java/com/staff/staffapp/MainActivity.java
+++ app/src/main/java/com/staff/staffapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.staff.staffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button mViewProductsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewProductsButton=(Button) findViewById(R.id.viewProductsButton);
+        mViewProductsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==mViewProductsButton){
+            Intent intent=new Intent(MainActivity.this,ProductLandingActivity.class);
+            startActivity(intent);
+        }
     }
 }
