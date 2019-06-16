3aad0b8951e39d9eb0b54ad95600e75797352e24
Robert Kagema Njiriri <kagemarobert@gmail.com>
2019-06-16 17:57:46 +0300

    [feature/news] - fix news button error

--- app/src/main/java/com/staff/staffapp/NewsActivity.java
+++ app/src/main/java/com/staff/staffapp/NewsActivity.java
@@ -1,6 +1,5 @@
 package com.staff.staffapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +9,5 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-
-        Intent intent = getIntent();
     }
 }
