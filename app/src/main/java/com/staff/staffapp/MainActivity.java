9e58e2510fd16976d9ab342728364de2ab55e318
naisenyamungai <naisenyamungai@gmail.com>
2019-06-16 19:36:03 +0300

    create FAQ activity

--- app/src/main/java/com/staff/staffapp/MainActivity.java
+++ app/src/main/java/com/staff/staffapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.staff.staffapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFAQButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFAQButton = (Button) findViewById(R.id.faq_button);
+        mFAQButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                
+            }
+        });
     }
 }
