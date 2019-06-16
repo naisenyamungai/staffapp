9c4ba754d815007f7e6ec2e45ab090ae1cd27163
Robert Kagema Njiriri <kagemarobert@gmail.com>
2019-06-16 19:08:26 +0300

    [feature/news] - add search bar to news activity

--- app/src/main/java/com/staff/staffapp/NewsActivity.java
+++ app/src/main/java/com/staff/staffapp/NewsActivity.java
@@ -2,13 +2,42 @@ package com.staff.staffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NewsActivity extends AppCompatActivity {
+
+    @BindView(R.id.newsListView) ListView mNewsList;
+    private String[] news = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, news);
+        mNewsList.setAdapter(adapter);
+
+        mNewsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(NewsActivity.this, NewsDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
