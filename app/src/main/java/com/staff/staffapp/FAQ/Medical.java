cb1a26bd3e4e4aa17b59ff0332b6d75fd826146d
naisenyamungai <naisenyamungai@gmail.com>
2019-06-18 10:42:43 +0300

    create medical activity

---
+++ app/src/main/java/com/staff/staffapp/FAQ/Medical.java
@@ -0,0 +1,16 @@
+package com.staff.staffapp.FAQ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.staff.staffapp.R;
+
+public class Medical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical);
+    }
+}
