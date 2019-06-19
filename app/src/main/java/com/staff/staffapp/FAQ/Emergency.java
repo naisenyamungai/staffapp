c792571f1970279c33b05cd8225cd036b2d85eab
naisenyamungai <naisenyamungai@gmail.com>
2019-06-19 20:33:01 +0300

    modify content in emergency.java file

--- app/src/main/java/com/staff/staffapp/FAQ/Emergency.java
+++ app/src/main/java/com/staff/staffapp/FAQ/Emergency.java
@@ -100,6 +100,7 @@ public class Emergency extends AppCompatActivity {
         List<String> test = new ArrayList<>();
         test.add("This is Sam Ngigi Number");
         test.add("0722 60 30 85");
+        test.add("0721 48 79 15");
 
 
         listHash.put(listDataHeader.get(0), safaricomSecurity);
