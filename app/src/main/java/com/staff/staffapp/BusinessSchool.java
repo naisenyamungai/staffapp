4b23ffc35213bfed53d46b12ccaf50b5ecb26844
developer <emakuthi@gmail.com>
2019-06-20 11:45:26 +0300

    add grid layout and cards

--- app/src/main/java/com/staff/staffapp/BusinessSchool.java
+++ app/src/main/java/com/staff/staffapp/BusinessSchool.java
@@ -1,22 +1,16 @@
 package com.staff.staffapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BusinessSchool extends AppCompatActivity {
-
     private ActionBar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_school);
-
         toolbar = getSupportActionBar();
         BottomNavigationView navigation = findViewById(R.id.navigation);
 
