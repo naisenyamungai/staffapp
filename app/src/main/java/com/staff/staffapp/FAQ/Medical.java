ff04eb19ec879f4f10458f3a0a23061d926aed6e
naisenyamungai <naisenyamungai@gmail.com>
2019-06-20 19:31:44 +0300

    add medical panelists per region

--- app/src/main/java/com/staff/staffapp/FAQ/Medical.java
+++ app/src/main/java/com/staff/staffapp/FAQ/Medical.java
@@ -2,23 +2,33 @@ package com.staff.staffapp.FAQ;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.staff.staffapp.R;
 
 public class Medical extends AppCompatActivity {
 
-    PDFView pdfView;
+    Button btn_attachment1, btn_attachment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical);
 
-        pdfView = findViewById(R.id.pdfview);
-        pdfView.fromAsset("medical(1).pdf")
-                .load();
+        btn_attachment1 = (Button) findViewById(R.id.attachment1);
+        btn_attachment2 = (Button) findViewById(R.id.attachment2);
 
+        btn_attachment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Medical.this, Panel.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/staff/staffapp/FAQ/Panel.java
+++ app/src/main/java/com/staff/staffapp/FAQ/Panel.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
 import com.staff.staffapp.R;
 
 public class Panel extends AppCompatActivity {
+    PDFView attachment1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel);
+
+        attachment1 = (PDFView) findViewById(R.id.pdfAttachment1);
+
+        attachment1.fromAsset("medical.pdf").load();
     }
 }
