4af0884adc91fa42933026cbdfcbcb61390f19e4
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-06-20 15:54:45 +0300

    created new activity in experiment and created list layouts

---
+++ app/src/main/java/com/staff/staffapp/ExpandableListAdapter.java
@@ -0,0 +1,13 @@
+package com.staff.staffapp;
+
+import android.content.Context;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>>
+}
---
+++ app/src/main/java/com/staff/staffapp/ProductsListActivity.java
@@ -0,0 +1,14 @@
+package com.staff.staffapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products_list);
+    }
+}
