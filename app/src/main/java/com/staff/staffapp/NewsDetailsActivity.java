476d03ea2c9c7bb9317298c7d57ee7d9125eaef7
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-21 10:47:52 +0300

    [feature/news] - will add Webview and Progressbar in Activity_news_details.xml, add Internet access permission in AndroidManifest.xml, and declare Webview and ProgressBar variables in NewsDetailsActivity

--- app/src/main/java/com/staff/staffapp/NewsDetailsActivity.java
+++ app/src/main/java/com/staff/staffapp/NewsDetailsActivity.java
@@ -2,13 +2,24 @@ package com.staff.staffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class NewsDetailsActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar detailsloader;
+    String url = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
     }
+
+//    Intent intent = getIntent();
+//    url = intent.getStringExtra("url");
+//    loader = (ProgressBar)
 }
