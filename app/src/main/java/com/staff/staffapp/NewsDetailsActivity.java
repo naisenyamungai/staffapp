2be09b38e7bbc6fc98044ad92c0ef606a763c528
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-21 17:30:08 +0300

    [feature/news] -will add app bar color change to ##00C858

--- app/src/main/java/com/staff/staffapp/NewsDetailsActivity.java
+++ app/src/main/java/com/staff/staffapp/NewsDetailsActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
 public class NewsDetailsActivity extends AppCompatActivity {
-    WebView webView;
-    ProgressBar detailsloader;
+    WebView webView_newsdetails;
+    ProgressBar loader_newsdetails;
     String url = "";
 
 
@@ -17,9 +19,27 @@ public class NewsDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
+
+
+    Intent intent = getIntent();
+    url = intent.getStringExtra("url");
+    loader_newsdetails = (ProgressBar) findViewById(R.id.loader_newsdetails);
+    webView_newsdetails = (WebView) findViewById(R.id.webView_newsdetails);
+    webView_newsdetails.getSettings().setBuiltInZoomControls(true);
+    webView_newsdetails.getSettings().setDisplayZoomControls(false);
+    webView_newsdetails.loadUrl(url);
+
+    webView_newsdetails.setWebChromeClient(new WebChromeClient() {
+       public void onProgressChanged(WebView view, int progress) {
+           if (progress == 100) {
+               loader_newsdetails.setVisibility(View.GONE);
+
+           } else {
+               loader_newsdetails.setVisibility(View.VISIBLE);
+           }
+        }
+    });
+
     }
 
-//    Intent intent = getIntent();
-//    url = intent.getStringExtra("url");
-//    loader = (ProgressBar)
 }
