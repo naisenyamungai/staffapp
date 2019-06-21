9eb168be2b25f0669f18f4dca45805435209f538
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-06-21 09:51:19 +0300

    created adapter for grid layout recycler view

---
+++ app/src/main/java/com/staff/staffapp/ProductLandingData.java
@@ -0,0 +1,13 @@
+package com.staff.staffapp;
+
+public class ProductLandingData {
+    private String name;
+
+    public ProductLandingData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
