a76aa6f5e7f47b6e8642087c4e8c049fafa4c003
naisenyamungai <naisenyamungai@gmail.com>
2019-06-23 19:41:51 +0300

    add bottom navigation button to jobs class for easy navigation back to main activity

--- app/src/main/java/com/staff/staffapp/FAQ/Jobs.java
+++ app/src/main/java/com/staff/staffapp/FAQ/Jobs.java
@@ -1,9 +1,14 @@
 package com.staff.staffapp.FAQ;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.staff.staffapp.MainActivity;
 import com.staff.staffapp.R;
 
 public class Jobs extends AppCompatActivity {
@@ -12,5 +17,22 @@ public class Jobs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobs);
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Intent i;
+
+                    switch(menuItem.getItemId()){
+                        case R.id.nav_home: i = new Intent(Jobs.this, MainActivity.class); startActivity(i); break;
+
+                    }
+                    return true;
+                }
+            };
 }
