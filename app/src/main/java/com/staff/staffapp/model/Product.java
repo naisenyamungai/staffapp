37b48c5f9caae1544b2e860159df2ef877687631
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-06-25 13:00:51 +0300

    added service, model amd constants

---
+++ app/src/main/java/com/staff/staffapp/model/Product.java
@@ -0,0 +1,31 @@
+package com.staff.staffapp.model;
+
+//import org.parceler.Parcel;
+//
+//@Parcel
+public class Product {
+    int id;
+    String title;
+    String description;
+
+    public Product() {
+    }
+
+    public Product(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
