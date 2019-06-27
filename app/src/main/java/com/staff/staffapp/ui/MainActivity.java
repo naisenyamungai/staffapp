3128a8375bf9ba7eaa40ca8d11a8d503df5c50a8
SamNgigi <sam.t.ngigi@gmail.com>
2019-06-27 16:04:11 +0300

    [bug fix] fixed main activity

--- app/src/main/java/com/staff/staffapp/ui/MainActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/MainActivity.java
@@ -9,14 +9,52 @@ import android.widget.Button;
 
 import com.staff.staffapp.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.staff.staffapp.faq.ProductsActivity;
+import com.staff.staffapp.school.BusinessSchool;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mFAQButton;
     Button mViewProductsButton;
+    Button btn;
+    @BindView(R.id.newsActivityButton)
+    Button mNewsActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn;
+        ButterKnife.bind(this);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BusinessSchool.class));
+            }
+        });
+
+        mFAQButton = (Button) findViewById(R.id.faq_button);
+
+        mFAQButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FAQ.class);
+                startActivity(intent);
+            }
+        });
+
+        mNewsActivityButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mViewProductsButton = (Button) findViewById(R.id.viewProductsButton);
         mViewProductsButton.setOnClickListener(this);
