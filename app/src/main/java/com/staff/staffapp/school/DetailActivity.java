f6ec222f9d5b2fb9253a18380179c05d93a6862d
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-06-27 19:42:54 +0300

    rebased working

--- app/src/main/java/com/staff/staffapp/school/DetailActivity.java
+++ app/src/main/java/com/staff/staffapp/school/DetailActivity.java
@@ -1,28 +1,21 @@
 package com.staff.staffapp.school;
 
-import androidx.viewpager.widget.ViewPager;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import com.staff.staffapp.R;
 
 public class DetailActivity extends AppCompatActivity {
 
-    ViewPager viewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
-
-        viewPager.setAdapter(viewPagerAdapter);
-
+        setContentView(R.layout.activity_detail);
     }
-
 }
+
 
 
