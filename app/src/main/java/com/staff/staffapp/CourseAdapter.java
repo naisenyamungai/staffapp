97a36f51f5fdb505b1ea2cebb9e38b5733ff86f6
Mwaniki <mr.mwaniki@gmail.com>
2019-06-27 09:10:29 +0300

    change the cards look

---
+++ app/src/main/java/com/staff/staffapp/CourseAdapter.java
@@ -0,0 +1,33 @@
+//package com.staff.staffapp;
+//
+//import android.content.Context;
+//import android.view.View;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.ArrayList;
+//
+//public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+//    private Context mContext;
+//    private ArrayList<CourseItem> mCourseLists;
+//
+//
+//
+//    public class CourseViewHolder extends RecyclerView.ViewHolder {
+//        public ImageView mImageView;
+//        public TextView mTextViewCourse;
+//
+//
+//        public CourseViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            mImageView = itemView.findViewById(R.id.info_image);
+//            mTextViewCourse= itemView.findViewById(R.id.info_text);
+//        }
+//    }
+//
+//
+//}
