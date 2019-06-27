21182a90c6d1c6342073ec9511fdd9a608474a8d
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-06-27 10:15:16 +0300

    added adapter, service, model and constants

---
+++ app/src/main/java/com/staff/staffapp/Constants.java
@@ -0,0 +1,6 @@
+package com.staff.staffapp;
+
+public class Constants {
+    public static final String BUSINESS_PRODUCTS_URL="https://fathomless-tor-95579.herokuapp.com/api/businesses";
+    public static final String PERSONAL_PRODUCTS_URL="https://fathomless-tor-95579.herokuapp.com/api/personals";
+}
---
+++ app/src/main/java/com/staff/staffapp/model/Product.java
@@ -0,0 +1,27 @@
+package com.staff.staffapp.model;
+
+public class Product {
+    int id;
+    String title;
+    String description;
+
+    public Product() { }
+
+    public Product(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
